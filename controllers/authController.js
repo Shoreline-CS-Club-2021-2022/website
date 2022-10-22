fed4fba88ac8df5591184b2b22242cb98acb584a
Charles Bither <95773999+CharlesBither@users.noreply.github.com>
2022-10-21 17:58:44 -0700

    added passport functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,44 @@
+const passport = require("passport");
+const User = require('../models/User');
+
 exports.login_page = (req, res) => {
     res.render('login');
 }
 
+exports.login = function (req, res) {
+    passport.authenticate("local", function (err, user, info) {
+        if (err) {
+            res.json({ success: false, message: err });
+        }
+        else {
+            if (!user) {
+                res.json({ success: false, message: info.message });
+            }
+            else {
+                res.json({ success: true, message: "Authentication successful"});
+            }
+        }
+    })(req, res);
+};
+
 exports.register_page = (req, res) => {
     res.render('register');
+}
+
+exports.register = function (req, res) {
+    User.register(new User({ username: req.body.username }), req.body.password, function (err, user) {
+        if (err) {
+            res.json({ success: false, message: "Your account could not be saved. Error: " + err });
+        }
+        else {
+            req.login(user, (er) => {
+                if (er) {
+                    res.json({ success: false, message: er });
+                }
+                else {
+                    res.json({ success: true, message: "Your account has been saved" });
+                }
+            });
+        }
+    });
 }
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,10 @@ const auth_controller = require("../controllers/authController");
 
 router.get("/login", auth_controller.login_page);
 
+router.post("/login/password", auth_controller.login);
+
 router.get("/register", auth_controller.register_page);
+
+router.post("/createUser", auth_controller.register);
 
 module.exports = router;
