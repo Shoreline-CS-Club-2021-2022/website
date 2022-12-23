f0b166df704b7442d5d030b839ae860696599036
Shawn Collinge <shawncollinge@gmail.com>
2022-12-23 20:59:09 +0000

    Add admin auth

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,11 +1,29 @@
 exports.admin_page = (req, res) => {
-    res.render('admin');
+    if (req.user?.isAdmin) {
+        res.render("admin", {
+            user: req.user
+        });
+    } else {
+        res.redirect("/auth/login");
+    }
 }
 
 exports.users_page = (req, res) => {
-    res.render('users');
+    if (req.user?.isAdmin) {
+        res.render("users", {
+            user: req.user
+        });
+    } else {
+        res.redirect("/auth/login");
+    }
 }
 
 exports.add_project_page = (req, res) => {
-    res.render('addProject');
+    if (req.user?.isAdmin) {
+        res.render("addProject", {
+            user: req.user
+        });
+    } else {
+        res.redirect("/auth/login");
+    }
 }
