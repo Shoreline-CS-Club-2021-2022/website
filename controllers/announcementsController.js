f5858e9285f2ee2c79e465927a9c6580c077a22c
Shawn Collinge <shawncollinge@gmail.com>
2022-12-25 18:56:44 +0000

    add mongo models to announcement page

--- controllers/announcementsController.js
+++ controllers/announcementsController.js
@@ -1,5 +1,14 @@
+const Announcement = require('../models/Announcement');
+
 exports.announcements_page = (req, res) => {
-    res.render('announcements', {
-        user: req.user
-    });
-}
+    Announcement.find({}, function (err, announcements) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('announcements', {
+                announcements: announcements,
+                user: req.user
+            });
+        }
+    })
+};
