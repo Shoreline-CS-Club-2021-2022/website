56f57daeb17762203d789944c540d3437c7c104a
Shawn Collinge <shawncollinge@gmail.com>
2022-12-26 18:57:09 +0000

    Move projects controllers from admincontroller to projectcontroller

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -3,3 +3,136 @@ exports.projects_page = (req, res) => {
         user: req.user
     });
 }
+
+exports.add_project_page = (req, res) => {
+    if (req.user?.isAdmin) {
+        res.render("addProject", {
+            user: req.user
+        });
+    } else {
+        res.redirect("/auth/login");
+    }
+}
+
+exports.edit_project = function (req, res) {
+    if (req.user?.isAdmin) {
+        Project.findOne({
+            _id: req.params.projectID
+        }, function (err, post) {
+            res.render("editProject", {
+                user: req.user,
+                project: post
+            });
+        });
+    } else {
+        res.redirect("/auth/login");
+    }
+};
+
+exports.new_project_post = function (req, res) {
+    if (req.user?.isAdmin) {
+        const project = new Project({
+            title: req.body.title,
+            githubLink: req.body.githubLink,
+            techStacks: req.body.techStacks,
+            demoLink: req.body.demoLink,
+            description: req.body.description,
+            images: req.files,
+        });
+        project.save();
+        res.redirect("addProject");
+    } else {
+        res.redirect("/auth/login");
+    }
+};
+
+exports.new_project_post = function (req, res) {
+    if (req.user?.isAdmin) {
+        const project = new Project({
+            title: req.body.title,
+            githubLink: req.body.githubLink,
+            techStacks: req.body.techStacks,
+            demoLink: req.body.demoLink,
+            description: req.body.description,
+            images: req.files,
+        });
+        project.save();
+        res.redirect("addProject");
+    } else {
+        res.redirect("/auth/login");
+    }
+};
+
+exports.edit_project_post = function (req, res) {
+    if (req.user?.isAdmin) {
+        Project.findOne({
+            _id: req.params.projectID
+        }, function (err, post) {
+            let images = post.images.concat(req.files);
+            Project.replaceOne({
+                _id: req.params.projectID
+            }, {
+                title: req.body.title,
+                githubLink: req.body.githubLink,
+                techStacks: req.body.techStacks,
+                demoLink: req.body.demoLink,
+                description: req.body.description,
+                images: images,
+            },
+                function (err, results) {
+                    if (!err) {
+                        res.redirect("/admin");
+                    } else {
+                        res.send(err);
+                    }
+                });
+
+        });
+    } else {
+        res.redirect("/auth/login");
+    }
+};
+
+exports.delete_project = function (req, res) {
+    if (req.user?.isAdmin) {
+      Project.deleteOne({
+        _id: req.params.projectID
+      }, function (err) {
+        if (!err) {
+          res.redirect(req.header('Referer'));
+        } else {
+          res.send(err);
+        }
+      });
+    } else {
+      res.redirect("/auth/login");
+    }
+  };
+  
+  exports.delete_image = function (req, res) {
+    if (req.user?.isAdmin) {
+      Project.findOne({
+        _id: req.params.projectID
+      }, function (err, post) {
+        if (err) {
+          console.log(err);
+        } else {
+          post.images.splice(req.params.imageIndex, 1);
+          Project.updateOne({
+            _id: req.params.projectID
+          }, {
+            images: post.images
+          },
+            function (err) {
+              if (!err) {
+                res.redirect(req.header('Referer'));
+              } else {
+                res.send(err);
+              }
+            });
+        }
+      });
+    } else {
+      res.redirect("/auth/login");
+    }
+  };
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const admin_controller = require("../controllers/adminController");
+const projects_controller = require("../controllers/projectsController");
 const multer = require('multer');
 
 const storage = multer.diskStorage({
@@ -19,11 +20,11 @@ router.get("/", admin_controller.admin_page);
 
 router.get("/users", admin_controller.users_page);
 
-router.get("/addProject",  upload.array("images", 12), admin_controller.add_project_page);
+router.get("/addProject",  upload.array("images", 12), projects_controller.add_project_page);
 
-router.post("/newproject", upload.array("images", 12), admin_controller.new_project_post);
-router.post("/edit/:projectID", upload.array("images", 12), admin_controller.edit_project_post);
-router.post("/delete/:projectID", admin_controller.delete_project);
-router.post("/delimage/:projectID/:imageIndex", admin_controller.delete_image);
+router.post("/newproject", upload.array("images", 12), projects_controller.new_project_post);
+router.post("/edit/:projectID", upload.array("images", 12), projects_controller.edit_project_post);
+router.post("/delete/:projectID", projects_controller.delete_project);
+router.post("/delimage/:projectID/:imageIndex", projects_controller.delete_image);
 
 module.exports = router;
